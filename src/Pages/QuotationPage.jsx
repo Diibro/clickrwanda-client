52ecb9649b6f565f817faccdbacc8c1c254ce398
Dushime Brother <dushimeconso1234@gmail.com>
2024-01-17 11:29:10 +0200

    updated the quotations page

--- src/Pages/QuotationPage.jsx
+++ src/Pages/QuotationPage.jsx
@@ -18,27 +18,27 @@ const QuotationPage = () => {
      const [loading, setLoading] =  useState(false);
      useEffect(() => {
           if(quoteName === 'request quotation') {
-               setMessage('RFQ is a solution offered by clickrwanda.com for our buyers seeking multiple quotes for specific products or services.');
+               setMessage('Get multiple quotes from different vendors. Your Request for Quotation (RFQ) will be shared to the different trusted sellers, and you receive your responses with 24 hours.');
                setQuotation(prev => ({...prev, quote_type: "quotation-request"}));
                setName("quotation");
           }
           else if(quoteName === 'find room') {
-               setMessage('');
+               setMessage('Whether you are looking for hotel , lodge , apartment or to rent a house, please complete the below information ');
                setQuotation(prev => ({...prev, quote_type: "find-room"}));
                setName("quotation");
           }
           else if(quoteName === 'buy house') {
-               setMessage('');
+               setMessage('If you are looking for buying a house, a plot or land in Rwanda, please complete the below information.');
                setQuotation(prev => ({...prev, quote_type: "buy-house"}));
                setName("quotation");
           }
           else if(quoteName === 'buy car') {
-               setMessage('');
+               setMessage('If you are looking for buying or renting a car in Rwanda, please complete the below information ');
                setQuotation(prev => ({...prev, quote_type: "buy-car"}));
                setName("quotation");
           }
           else if(quoteName === 'find job') {
-               setMessage('');
+               setMessage('If you are looking for a job in Rwanda, Please Fill out the below form.');
                setQuotation(prev => ({...prev, quote_type: "find-job"}));
                setName("job details");
           }
@@ -48,9 +48,8 @@ const QuotationPage = () => {
           e.preventDefault();
           try {
                setLoading(true);
-               console.log(quotation.file);
                const formData = new FormData();
-               formData.append('file', quotation.file);
+               if(quotation.file) formData.append('file', quotation.file);
                formData.append('email', quotation.email);
                formData.append('phone', quotation.phone);
                formData.append('description', quotation.description);
@@ -85,7 +84,7 @@ const QuotationPage = () => {
                          loading ? <Loading /> :
                          submitted ? <p className="form-submitted">You have successfully submitted the request</p> :
                          <>
-                              <p>Complete the following form about your {name}.</p>
+                              <p>Complete the following form or call <a href="tel:+250788490887">+250 788490887</a> / <a href="tel:+250 727559173">+250 727559173</a></p>
                               <form onSubmit={async (e) => await handleSubmit(e)}>
                               <div className="group">
                                    <label htmlFor="email-03">Email:</label>
@@ -99,12 +98,17 @@ const QuotationPage = () => {
                                    <label htmlFor="description-03">Description:</label>
                                    <textarea className="input" name="description-03" id="description-03" cols="30" rows="15" placeholder="I am looking for..." required onChange={e => setQuotation(prev => ({...prev, description:e.target.value}))}></textarea>
                               </div>
+                              {
+                                   quoteName === "request quotation" ? 
+                                   <div className="group">
+                                        <label htmlFor="quote-detail">Upload File</label>
+                                        <input type="file" name="quote-file" id="quote-file"  required onChange={e => setQuotation(prev => ({...prev, file:e.target.files[0]}))} />
+                                   </div>
+                                   :
+                                   null
+                              }
                               <div className="group">
-                                   <label htmlFor="quote-detail">Upload File</label>
-                                   <input type="file" name="quote-file" id="quote-file"  required onChange={e => setQuotation(prev => ({...prev, file:e.target.files[0]}))} />
-                              </div>
-                              <div className="group">
-                                   <p><input type="checkbox" required /> I agree to share my {name === "job details" ? "CV" : ''} document with the quoted suppliers </p>
+                                   { quoteName === "request quotation" ? <p><input type="checkbox" required /> I agree to share my Request For Quotaion (RFQ) document with the quoted suppliers </p> : null}
                                    <p><input type="checkbox" required /> I have read, understood and agreed <Link to='/terms-&-conditions'>Terms and Conditions </Link> of Click Rwanda</p>
                               </div>
                               <div className="group">
