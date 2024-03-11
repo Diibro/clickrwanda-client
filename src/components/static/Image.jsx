e235a5683adfbc928ffa8bfbf204abd37625cdbe
Dushime Brother <dushimeconso1234@gmail.com>
2024-03-11 21:16:17 +0200

    updated the advert view

--- src/components/static/Image.jsx
+++ src/components/static/Image.jsx
@@ -2,9 +2,26 @@
 import PropTypes from "prop-types";
 
 export const CImage = ({image}) => {
+     const imageStyle = {
+          // width: image.width || "100%",
+          // height: image.height || "100%",
+          maxWidth: image.maxWidth || "100%",  // Add maxWidth property
+          maxHeight: image.maxHeight || "100%",
+          borderBottom: "1px solid gray",
+          zIndex: "1"
+     }
+     const containerStyles= {
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center"
+     }
      return(
-          <div className="c-image">
-               <img src={image.src} width={image.width || "100%"} height={image.height || "100%"} alt={image.alt} loading="lazy" onClick={image.action} />
+          <div className="c-image" style={containerStyles}>
+               {/* <img src={image.src} width={image.width || "100%"} height={image.height || "100%"} alt={image.alt} loading="lazy" onClick={image.action} /> */}
+               <img src={image.src}  alt={image.alt} loading="lazy" style={imageStyle}  onClick={image.action} />
           </div>
      )
 }
