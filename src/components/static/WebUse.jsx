210e8754c02d22373f547a7d9c2d7ab744cdacbb
Dushime Brother <dushimeconso1234@gmail.com>
2024-03-20 07:12:36 +0200

    added the website visits section

---
+++ src/components/static/WebUse.jsx
@@ -0,0 +1,20 @@
+
+const WebUse = () => {
+     return (
+          <div className="web-usage-container">
+               <h2>Website Visits</h2>
+               <div className="visits">
+                    <h3>Today: 100</h3>
+                    
+               </div>
+               <div className="visits">
+                    <h3>This Month: 20,000</h3>
+               </div>
+               <div className="visits">
+                    <h3>This year: 40,000</h3>
+               </div>
+          </div>
+     )
+}
+
+export default WebUse
