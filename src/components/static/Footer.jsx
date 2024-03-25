d780a18cb31bb5d86f70d402a5dd6d75d6e6ed31
Dushime Brother <dushimeconso1234@gmail.com>
2024-03-25 06:55:59 +0200

    added the google analytics scripts

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = () => {
                return navigate("/forms/signup")
           }
      }
-     // const viewPlans = () => {
-     //      // return navigate('/');
-     //      return navigate('/payment-plans');
-     // }
+     const viewPlans = () => {
+          // return navigate('/');
+          return navigate('/payment-plans');
+     }
      return (
      <div className='footer'>
           <div className="footer-col">
@@ -45,7 +45,7 @@ const Footer = () => {
                     <a href="https://wa.me/+250 787260494" rel="noreferrer" target="_blank"><i className="icon whatsapp-icon"><FaWhatsapp /></i> Whatsapp: +250787260494 </a>
                </div>
                {!loggedIn ? <ActionBtn title="Get Started" action={getStarted} /> : null } 
-               {/* <ActionBtn title="Our Plans"  action={viewPlans}/> */}
+               <ActionBtn title="Our Plans"  action={viewPlans}/>
           </div>
           <div className="footer-col">
                <Title content={{color:textColors.white, size: titleSize.small, name: "Support & Links"}} />
