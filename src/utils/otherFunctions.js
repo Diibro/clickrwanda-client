ddcf87e7c24e432120614e7c32ffc8ff08deed5b
Dushime Brother <dushimeconso1234@gmail.com>
2024-06-09 10:12:41 +0200

    updates

--- src/utils/otherFunctions.js
+++ src/utils/otherFunctions.js
@@ -15,10 +15,14 @@ export const charAdder = (names, char) => {
 }
 
 export const capitalizeString =(text) => {
-     const words = text.split(' ');
-     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-     const result = capitalizedWords.join(' ');
-     return result;
+      if(text){
+        const words = text.split(' ');
+        const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+        const result = capitalizedWords.join(' ');
+        return result;
+      }else{
+        return text
+      }
 }
 
 export const formatPriceV1 = (price) => {
