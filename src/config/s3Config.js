b28e0548045f9cb1910d51f4044290519dd5ddf3
Dushime Brother <dushimeconso1234@gmail.com>
2024-06-30 00:21:30 +0200

    removed aws sdk

--- src/config/s3Config.js
+++ src/config/s3Config.js
@@ -17,17 +17,17 @@ export const s3Folders = {
 }
 
 // aws-config.js
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-// import { CognitoIdentityServiceProvider } from 'aws-sdk';
+// // import { CognitoIdentityServiceProvider } from 'aws-sdk';
 
-AWS.config.update({
-     accessKeyId: "AKIA6ODVAPPY43PDXM4L",
-     secretAccessKey: "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",
-     region: "eu-north-1",
-});
+// AWS.config.update({
+//      accessKeyId: "AKIA6ODVAPPY43PDXM4L",
+//      secretAccessKey: "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",
+//      region: "eu-north-1",
+// });
 
-const s3 = new AWS.S3();
-// const s3 = new CognitoIdentityServiceProvider.S3();
+// const s3 = new AWS.S3();
+// // const s3 = new CognitoIdentityServiceProvider.S3();
 
-export default s3;
+// export default s3;
--- src/utils/aws-upload-functions.js
+++ src/utils/aws-upload-functions.js
@@ -1,58 +1,61 @@
 // upload.js
 
 import { v4 as uuidv4 } from 'uuid';
-import s3 from "../config/s3Config";
+// import s3 from "../config/s3Config";
 
 const uploadFile = async (file, folder) => {
-     const uniqueKey = `${uuidv4()}-${file.name}`;
-     const filePath = `${folder}/${uniqueKey}`
-     const params = {
-          Bucket: "clickrwanda.s3.com",
-          Key: filePath,
-          Body: file,
-          ACL: 'public-read', // Optional: for public access
-          ContentType: file.type,
-     };
+     // const uniqueKey = `${uuidv4()}-${file.name}`;
+     // const filePath = `${folder}/${uniqueKey}`
+     // const params = {
+     //      Bucket: "clickrwanda.s3.com",
+     //      Key: filePath,
+     //      Body: file,
+     //      ACL: 'public-read', // Optional: for public access
+     //      ContentType: file.type,
+     // };
 
-     const response = await s3.upload(params).promise();
-     const {Location} = response;
-     return Location;
+     // const response = await s3.upload(params).promise();
+     // const {Location} = response;
+     // return Location;
+     return "";
 };
 
 export const deleteFile = async (fileUrl) => {
-     const arr = fileUrl.split("/");
+     // const arr = fileUrl.split("/");
 
-     const fileName = arr[arr.length - 1];
-     const params = {
-          Bucket: "clickrwanda.s3.com",
-          Key: fileName,
-     };
+     // const fileName = arr[arr.length - 1];
+     // const params = {
+     //      Bucket: "clickrwanda.s3.com",
+     //      Key: fileName,
+     // };
 
-     try {
-          const data = await s3.deleteObject(params).promise();
-          console.log("File deleted successfully:", data);
-          return data;
-     } catch (error) {
-          console.error("Error deleting file:", error);
-          throw error;
-     }
+     // try {
+     //      // const data = await s3.deleteObject(params).promise();
+     //      console.log("File deleted successfully:", data);
+     //      return data;
+     // } catch (error) {
+     //      console.error("Error deleting file:", error);
+     //      throw error;
+     // }
+
+     return null
 };
 
 export const uploadMany = async (files, folder) => {
      const fileUrls = [];
      
-          if (files && files[0]) {
-          const uploadPromises = Array.from(files).map(async (file) => {
-               try {
-                    const fileUrl = await uploadFile(file, folder);
-                    fileUrls.push(fileUrl);
-               } catch (error) {
-                    console.log(error);
-               }
-          });
+          // if (files && files[0]) {
+          // const uploadPromises = Array.from(files).map(async (file) => {
+          //      try {
+          //           const fileUrl = await uploadFile(file, folder);
+          //           fileUrls.push(fileUrl);
+          //      } catch (error) {
+          //           console.log(error);
+          //      }
+          // });
      
-          await Promise.all(uploadPromises);
-          }
+          // await Promise.all(uploadPromises);
+          // }
      
           return fileUrls;
 };
