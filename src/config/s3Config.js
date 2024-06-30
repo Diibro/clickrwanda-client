46581b7dffbcf699ac5c5772217c4f70f16b0417
Dushime Brother <dushimeconso1234@gmail.com>
2024-06-30 07:08:38 +0200

    added aws sdk functionality

--- src/config/s3Config.js
+++ src/config/s3Config.js
@@ -1,20 +1,20 @@
-export const s3Config =  {
-     accessKeyId: "AKIA6ODVAPPY43PDXM4L",
-     secretAccessKey: "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",
-     bucketName: "clickrwanda.s3.com",
-     dirName: "adverts",
-     region: "eu-north-1",
-     s3Url: "https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/"
-}
+// export const s3Config =  {
+//      accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+//      secretAccessKey: "orvN1NiJ9eTnyvzLsI5S5/SUaXnNQvhhhaF7jOwb",
+//      bucketName: "clickrwanda.s3.com",
+//      dirName: "adverts",
+//      region: "eu-north-1",
+//      s3Url: "https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/"
+// }
 
-export const s3Folders = {
-     adverts: "adverts",
-     banners: "banners",
-     categories: "categories",
-     logos: "logos",
-     payPlans: "pay-plans",
-     quotations: "quotations"
-}
+// export const s3Folders = {
+//      adverts: "adverts",
+//      banners: "banners",
+//      categories: "categories",
+//      logos: "logos",
+//      payPlans: "pay-plans",
+//      quotations: "quotations"
+// }
 
 // aws-config.js
 // import AWS from 'aws-sdk';
@@ -31,3 +31,29 @@ export const s3Folders = {
 // // const s3 = new CognitoIdentityServiceProvider.S3();
 
 // export default s3;
+
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+     accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+     secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+     region: import.meta.env.VITE_REGION,
+});
+
+export const s3Config = {
+     bucketName: import.meta.env.VITE_BUCKET_NAME,
+     dirName: 'adverts',
+     region: import.meta.env.VITE_REGION,
+     s3Url: `https://s3.${import.meta.env.VITE_REGION}.amazonaws.com/${import.meta.env.VITE_BUCKET_NAME}/`,
+};
+
+export const s3Folders = {
+     adverts: 'adverts',
+     banners: 'banners',
+     categories: 'categories',
+     logos: 'logos',
+     payPlans: 'pay-plans',
+     quotations: 'quotations'
+};
+
+export default s3;
--- src/utils/aws-upload-functions.js
+++ src/utils/aws-upload-functions.js
@@ -1,61 +1,67 @@
 // upload.js
 
 import { v4 as uuidv4 } from 'uuid';
-// import s3 from "../config/s3Config";
+import s3, { s3Config } from "../config/s3Config";
 
 const uploadFile = async (file, folder) => {
-     // const uniqueKey = `${uuidv4()}-${file.name}`;
-     // const filePath = `${folder}/${uniqueKey}`
-     // const params = {
-     //      Bucket: "clickrwanda.s3.com",
-     //      Key: filePath,
-     //      Body: file,
-     //      ACL: 'public-read', // Optional: for public access
-     //      ContentType: file.type,
-     // };
+     const uniqueKey = `${uuidv4()}-${file.name}`;
+     const filePath = `${folder}/${uniqueKey}`
+     const params = {
+          Bucket: s3Config.bucketName,
+          Key: filePath,
+          Body: file,
+          ACL: 'public-read', // Optional: for public access
+          ContentType: file.type,
+     };
 
-     // const response = await s3.upload(params).promise();
-     // const {Location} = response;
-     // return Location;
-     return "";
+     // eslint-disable-next-line no-useless-catch
+     try {
+          const response = await s3.upload(params).promise();
+          const {Location} = response;
+          return Location;
+     } catch (error) {
+          throw error
+     }
+     
+     // return "";
 };
 
 export const deleteFile = async (fileUrl) => {
-     // const arr = fileUrl.split("/");
+     const arr = fileUrl.split("/");
 
-     // const fileName = arr[arr.length - 1];
-     // const params = {
-     //      Bucket: "clickrwanda.s3.com",
-     //      Key: fileName,
-     // };
+     const fileName = arr[arr.length - 1];
+     const params = {
+          Bucket: "clickrwanda.s3.com",
+          Key: fileName,
+     };
 
-     // try {
-     //      // const data = await s3.deleteObject(params).promise();
-     //      console.log("File deleted successfully:", data);
-     //      return data;
-     // } catch (error) {
-     //      console.error("Error deleting file:", error);
-     //      throw error;
-     // }
+     try {
+          const data = await s3.deleteObject(params).promise();
+          console.log("File deleted successfully:", data);
+          return data;
+     } catch (error) {
+          console.error("Error deleting file:", error);
+          throw error;
+     }
 
-     return null
+     // return null
 };
 
 export const uploadMany = async (files, folder) => {
      const fileUrls = [];
      
-          // if (files && files[0]) {
-          // const uploadPromises = Array.from(files).map(async (file) => {
-          //      try {
-          //           const fileUrl = await uploadFile(file, folder);
-          //           fileUrls.push(fileUrl);
-          //      } catch (error) {
-          //           console.log(error);
-          //      }
-          // });
+          if (files && files[0]) {
+          const uploadPromises = Array.from(files).map(async (file) => {
+               try {
+                    const fileUrl = await uploadFile(file, folder);
+                    fileUrls.push(fileUrl);
+               } catch (error) {
+                    console.log(error);
+               }
+          });
      
-          // await Promise.all(uploadPromises);
-          // }
+          await Promise.all(uploadPromises);
+          }
      
           return fileUrls;
 };
