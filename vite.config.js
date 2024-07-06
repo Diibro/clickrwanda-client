826d194df34cc175d0d616c844573f67ea69eb64
Dushime Brother <dushimeconso1234@gmail.com>
2024-07-06 16:09:38 +0200

    removed react snap

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// })
-
-//new
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    sourcemap: true,
   },
-});
+})
+
+//new
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'build',
+//   },
+// });
