1c98db834a9ced9af38d2264cf766d4ffae0aeb3
Dushime Brother <dushimeconso1234@gmail.com>
2024-10-01 18:26:59 +0200

    updated the banners

--- src/config/banners.js
+++ src/config/banners.js
@@ -1,18 +1,18 @@
 export const Banners = [
      {
+          name:"Green Application open",
+          images:{
+               hr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/visa-pop.gif",
+               vr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/aska-vr.png"
+          },
+          link: "https://clickrwanda.com/"
+     },
+     {
           name:"Visit My Venue",
           images:{
                hr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/visitmyvenue_hr-banner.gif",
                vr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/visitmyvenue-banner-vr.gif"
           },
           link: "https://visitmyvenue.com"
-     },
-     {
-          name:"Aska Logistics",
-          images:{
-               hr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/ask-hr-800-200.gif",
-               vr:"https://s3.eu-north-1.amazonaws.com/clickrwanda.s3.com/banners/aska-vr.png"
-          },
-          link: "https://askalogistics.com/"
      }
 ]
