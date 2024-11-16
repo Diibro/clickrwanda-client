8303161e362e634891c2feb2cecef86b0c3006cc
Dushime Brother <brother.dushime@gmail.com>
2024-11-16 09:06:08 +0200

    updated the home locations section

--- src/components/containers/HomeLocationsSection.jsx
+++ src/components/containers/HomeLocationsSection.jsx
@@ -30,15 +30,15 @@ const HomeLocationsSection = () => {
           <>
                {
                     locations && locations.length ?
-                         <div className="w-full flex flex-col  bg-main-blue-700 rounded-[10px] p-[5px] md:p-[20px] gap-[5px] md:gap-[20px] ">
+                         <div className="w-full flex flex-col  bg-main-blue-700 rounded-[10px] p-[20px]  gap-[20px] ">
                               <div className="w-full flex flex-col gap-[5px]">
                                    <h3 className="text-[1.4rem] font-extrabold text-gray-50 ">Ads per Area</h3>
                                    <p className="text-[0.8rem] text-gray-200   ">Discover products, services, best shops, and promotions in your locality!</p>
                               </div>
-                              <div className="w-full grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-[20px]  ">
+                              <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-[20px]  ">
                                    {
                                         newLocations ? 
-                                        newLocations.map((location, index) => <span className="w-full flex gap-[5px] text-[0.8rem] text-gray-100  cursor-pointer group" key={`home-section-location-${index}`} onClick={() => navigate(`/location?=${location?.name}`)}><i className="text-main-blue-600 group-hover:animate-spin"><FaLocationCrosshairs /></i> {location.name} <b className="text-[0.6rem] md:text-[0.7rem] font-mono text-main-blue-600 ">({location.count} ads)</b></span>)
+                                        newLocations.map((location, index) => <span className="w-full flex items-center gap-[5px] text-[0.8rem] text-gray-100  cursor-pointer group" key={`home-section-location-${index}`} onClick={() => navigate(`/location?=${location?.name}`)}><i className="text-main-blue-600 group-hover:animate-spin"><FaLocationCrosshairs /></i> {location.name} <b className="text-[0.7rem] font-mono text-main-blue-600 ">({location.count} ads)</b></span>)
                                         :locations.map((location, index) => <span className="text-[0.8rem] text-gray-100  " key={`home-section-location-${index}`} onClick={() => navigate(`/location?=${location}`)}>{location}</span>)
                                    }
                               </div>
