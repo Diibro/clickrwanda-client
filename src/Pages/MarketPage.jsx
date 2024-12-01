b981116a9b8af7aea441de138f30384f4663dd24
Dushime Brother <brother.dushime@gmail.com>
2024-12-01 11:33:42 +0200

    updated the marketplace page

--- src/Pages/MarketPage.jsx
+++ src/Pages/MarketPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import {  Route, Routes,   } from 'react-router-dom';
 import AdvertService from '../services/Advert'
-import { VerticalAds } from '../components/dynamic/Adverts.component';
-import { RightBanner } from '../components/dynamic/Banners';
-import { Banners } from '../config/banners';
+// import { VerticalAds } from '../components/dynamic/Adverts.component';
+// import { RightBanner } from '../components/dynamic/Banners';
+// import { Banners } from '../config/banners';
+import { GeneralAdsContainer } from '../components/containers/AdsContainer';
 
 const MarketPage = () => {
      
@@ -17,7 +18,7 @@ const MarketPage = () => {
 const MainPage = () => {
      
      return (
-          <div className="page">
+          <div className="w-full flex flex-col items-center gap-[10px]">
                <MarketPageHeader />
                <AdsContainer />
           </div>
@@ -26,9 +27,9 @@ const MainPage = () => {
 
 const MarketPageHeader = () => {
      return (
-          <div className="market-page-title">
-               <h1>Welcome to our Marketplace</h1>
-               <p>Buy best rated products on clickrwanda Market Place</p>
+          <div className="w-full bg-main-blue-700 flex flex-col items-center gap-[10px] py-[20px] rounded-[5px]">
+               <h1 className='text-[1.6rem] font-bold text-gray-50 text-center '>Welcome to our Marketplace</h1>
+               <p className='text-[0.8rem] text-gray-400 text-center '>Buy best rated products on clickrwanda Market Place</p>
           </div>
      )
 }
@@ -37,12 +38,15 @@ const MarketPageHeader = () => {
 const AdsContainer = () => {
      
      const [ads,setAds] = useState(null);
+     const [loading,setLoading] = useState(false);
 
      const fetchAds = async () => {
+          setLoading(true);
           const res = await AdvertService.getClientApprovedCommissionAds({limit:200, offset:0});
           if(res && res.data){
                setAds(res.data);
           }
+          setLoading(false);
      }
 
 
@@ -52,20 +56,14 @@ const AdsContainer = () => {
 
      
      return(
-          <div className="page-main">
-               <div className="side"></div>
-               <div className="page-content">
-                    {
-                         ads && ads.length ? 
-                         <VerticalAds ads={ads} adsNo={50} eleId={'commission-ads-container'}  />
-                         :
-                         <p className='no-ads-found'>No ads found</p>
-                    }
-               </div>
-               <div className="side right-side">
-                    <RightBanner items={Banners} />
-               </div>
-               
+          <div className="w-full flex flex-col items-center gap-[10px]">
+               {
+                    loading ? <p className='text-[0.9rem] font-mono text-gray-800'>Loading...</p> :
+                    ads && ads.length ? 
+                    <GeneralAdsContainer ads={ads} containerId={'commission-ads-container'}  />
+                    :
+                    <p className='no-ads-found'>No ads found</p>
+               }
           </div>
      )
 }
