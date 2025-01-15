c340f628801c9a1684dccef0f462b2796f1a2029
Dushime Brother <brother.dushime@gmail.com>
2025-01-15 13:36:33 +0200

    updated the mobile header view

--- src/components/static/Header.jsx
+++ src/components/static/Header.jsx
@@ -6,7 +6,6 @@ import DeviceView from "../../Contexts/ViewContext";
 // import AppData from "../../Contexts/AppContext";
 const profileImage = 'https://res.cloudinary.com/dyjahjf1p/image/upload/v1700982042/clickrwanda/logos/account_msinv8.png';
 import Logo from '../../assets/logo/clickrwanda.png';
-import { useTranslation } from "react-i18next";
 
 import { LuMenu } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
@@ -103,6 +102,7 @@ const MobileView = () => {
      const [navOn, setNavOn] = useState(false);
      const navigate = useNavigate();
      const agentToken = sessionStorage.getItem("agentToken");
+     const [showNav, setShowNav] = useState(false);
      const showHeader = () => {
           let navbar = document.getElementById("dashboard-nav-bar");
           if(loggedIn && (isMobile || isTablet)){
@@ -121,11 +121,23 @@ const MobileView = () => {
           <header className=" lg:hidden sticky top-0 bg-main-blue-700 px-[0.5%] py-[5px] flex items-center justify-between  rounded-[5px] my-[5px] z-50 ">
                <Link to='/'><img src={Logo} alt="clickrwanda" className="w-[100px] " /></Link>
                <div className="flex items-center gap-[2.5px]">
-                    
-                    {!loggedIn && !agentToken ? <ActionBtn action={() => navigate('/forms')} title={"Login"} /> : null}
+                    {/* <LanguageChanger /> */}
+                    <i className="text-gray-100 text-[36px] cursor-pointer group-hover:text-main-gold-500 transition-all duration-300" onClick={() => setShowNav(prev => !prev)} >{showNav ? <IoClose /> :<LuMenu />}</i>
                     {loggedIn || agentToken ? <Link onClick={showHeader} to={ userInfo?.user_type === 'seller' ? "/user-dashboard" : userInfo?.user_type === 'job-seeker' ? "/job-seeker" : agentToken ? "/agent" : "/admin"} className="w-[40px] aspect-square rounded-full p-[2px] border-[1px] border-blue-300 "><img className="w-full h-full rounded-full" src={userInfo?.profile_image || profileImage} alt="" /></Link> : null}
                     {!loggedIn || !agentToken ? <ActionBtn action={() => navigate('/business')} title={isTablet || isMobile ? "LIST YOUR BUSINESS / AD": "LIST YOUR BUSINESS / AD" } /> : null}
                </div>
+
+               {
+                    showNav &&
+                    <nav className="w-full absolute top-[105%] left-0 bg-white p-[5px] z-10 flex flex-col items-start gap-[2.5px] transition-all duration-300 ">
+                         {
+                              NavLinks.map((link, index) => 
+                                   <span onClick={() => navigate(link.link)} key={`desk-navbar-${index}`} className="text-[0.7rem] w-full text-gray-800 font-medium cursor-pointer group py-[5px] px-[10px] hover:bg-gray-200 rounded-[5px] transition-all duration-200 " >{link.name}</span>
+                              )
+                         }
+                         {!loggedIn && !agentToken ? <ActionBtn action={() => navigate('/forms')} title={"Login"} /> : null}
+                    </nav>
+               }
           </header>
      )
 }
