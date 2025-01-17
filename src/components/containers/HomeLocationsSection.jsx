a37916b3034e1ff97f767361fcde46e765a38601
Dushime Brother <brother.dushime@gmail.com>
2025-01-17 15:42:21 +0200

    updated the home locations section

--- src/components/containers/HomeLocationsSection.jsx
+++ src/components/containers/HomeLocationsSection.jsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react"
-import { getLocations } from "../../utils/locations";
+import { getLocations, locationImages } from "../../utils/locations";
 import { useNavigate } from "react-router-dom";
-import advertService from "../../services/Advert";
-import { FaLocationCrosshairs } from "react-icons/fa6";
+import { FaArrowRight, FaPlus, } from "react-icons/fa6";
+import PropTypes from "prop-types";
 
 const HomeLocationsSection = () => {
      const [locations, setLocations] = useState(null);
-     const [newLocations,setNewLocations] = useState(null);
-     const navigate = useNavigate();
-     const fetchLocationCounts = async(locations) => {
-          const res = await advertService.getCountsByLocation(locations);
-          if(res){
-               const data = res.data;
-               if(data && data.length){
-                    setNewLocations(data);
-               }
-          }
+     const [locsNo, setLocsNo]= useState(6);
+     // const [newLocations,setNewLocations] = useState(null);
+     // const fetchLocationCounts = async(locations) => {
+     //      const res = await advertService.getCountsByLocation(locations);
+     //      if(res){
+     //           const data = res.data;
+     //           if(data && data.length){
+     //                setNewLocations(data);
+     //           }
+     //      }
+     // }
+
+     const updateLocsNumber = () => {
+          return setLocsNo(prev => {
+               if(prev >= locations.length) return 6;
+               else return prev + 6;
+          })
      }
      useEffect(() => {
           (async() => {
@@ -23,30 +30,59 @@ const HomeLocationsSection = () => {
                // const {data} = districts;
                // setLocations(data);
                setLocations(districts);
-               await fetchLocationCounts(districts);
+               // await fetchLocationCounts(districts);
           })();
      },[])
      return (
           <>
                {
                     locations && locations.length ?
-                         <div className="w-full flex flex-col  bg-main-blue-700 rounded-[10px] p-[20px]  gap-[20px] ">
+                         <div className="w-full flex flex-col bg-main-blue-700 rounded-[10px] p-[20px]  gap-[20px] ">
                               <div className="w-full flex flex-col gap-[5px]">
-                                   <h3 className="text-[1.4rem] font-extrabold text-gray-50 ">Ads per Area</h3>
+                                   <h3 className="text-[1.4rem] font-extrabold text-gray-50 ">Popular Locations</h3>
                                    <p className="text-[0.8rem] text-gray-200   ">Discover products, services, best shops, and promotions in your locality!</p>
                               </div>
-                              <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-[20px]  ">
-                                   {
+                              <div className="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-[10px]  ">
+                                   {/* {
                                         newLocations ? 
                                         newLocations.map((location, index) => <span className="w-full flex items-center gap-[5px] text-[0.8rem] text-gray-100  cursor-pointer group" key={`home-section-location-${index}`} onClick={() => navigate(`/location?=${location?.name}`)}><i className="text-main-gold-500 group-hover:animate-spin"><FaLocationCrosshairs /></i> {location.name} <b className="text-[0.7rem] font-mono text-main-gold-500 ">({location.count} ads)</b></span>)
                                         :locations.map((location, index) => <span className="text-[0.8rem] text-gray-100  " key={`home-section-location-${index}`} onClick={() => navigate(`/location?=${location}`)}>{location}</span>)
+                                   } */}
+
+                                   {
+                                        locations.map((location, index) => locsNo >= index + 1 && <PopularLocationCard location={{name:location, image: locationImages[`${location.toLowerCase()}`]}} key={`home-location-card-${index}`} />)
                                    }
+                              </div>
+                              <div className="w-full flex items-center justify-end">
+                                   <button className="w-auto px-[20px] py-[7px] text-[0.8rem] text-white bg-main-gold-500 rounded-[5px] inline-flex items-center gap-[5px]" onClick={updateLocsNumber}>{locsNo < locations.length ? <><FaPlus /> Show More</> : "Show less"}</button>
                               </div>
                          </div>
                     : null
                }
           </>
      )
+}
+
+const PopularLocationCard = ({location}) => {
+     const navigate = useNavigate();
+     return(
+          <div className="w-full h-auto overflow-hidden grid grid-cols-2 md:grid-cols-1 rounded-[5px] bg-gray-100 p-[5px]">
+               <div className="w-full h-[100px]">
+                    <img className="w-full h-full object-cover rounded-[5px]" src={location.image} alt={location.name} />
+               </div>
+               <div className="w-full h-full flex flex-col items-center justify-center p-[5px] ">
+                    <h4 className="text-[1rem] text-main-blue-700 font-bold ">{location.name}</h4>
+                    <span onClick={() => navigate(`/location?=${location?.name}`)} className="text-[0.8rem] cursor-pointer text-blue-600 flex items-center gap-[3px]"><b>View ads</b> <i className="text-[18px]"><FaArrowRight /></i></span>
+               </div>
+          </div>
+     )
+} 
+
+PopularLocationCard.propTypes = {
+     location: PropTypes.shape ({
+          name: PropTypes.string,
+          image: PropTypes.string
+     })
 }
 
 export default HomeLocationsSection
--- src/utils/locations.js
+++ src/utils/locations.js
@@ -1,40 +1,73 @@
-import axios from "axios";
-const api = 'https://rwanda.p.rapidapi.com/';
+
+// const api = 'https://rwanda.p.rapidapi.com/';
 import locations from "../data/Location.json";
 
-const provincesOptions = {
-  method: 'GET',
-  url: `${api}provinces`,
-  headers: {
-    'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
-    'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
-  }
-};
+// const provincesOptions = {
+//   method: 'GET',
+//   url: `${api}provinces`,
+//   headers: {
+//     'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
+//     'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
+//   }
+// };
 
-const allLocationsOptions = {
-     method: 'GET',
-     url: `${api}provinces`,
-     headers: {
-       'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
-       'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
-     }
-   };
-const districtsOptions = {
-     method: 'GET',
-     url: `${api}districts`,
-     headers: {
-       'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
-       'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
-     }
-   };
+// const allLocationsOptions = {
+//      method: 'GET',
+//      url: `${api}provinces`,
+//      headers: {
+//        'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
+//        'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
+//      }
+//    };
+// const districtsOptions = {
+//      method: 'GET',
+//      url: `${api}districts`,
+//      headers: {
+//        'X-RapidAPI-Key': '54bd11d350msh57b6121d40064c5p1ac709jsnfa82ce0de082',
+//        'X-RapidAPI-Host': 'rwanda.p.rapidapi.com'
+//      }
+//    };
 
 
 
 
 export const getLocations = () => {
-     try {
-          return locations
-     } catch (error) {
-          console.error(error);
-     }
+      try {
+            return locations
+      } catch (error) {
+            console.error(error);
+      }
 }
+
+export const locationImages = {
+  bugesera: "/districts/bugesera.jpg",
+  gatsibo: "/districts/gatsibo.jpg",
+  kayonza: "/districts/kayonza.jpeg",
+  kirehe: "/districts/kirehe.webp",
+  ngoma: "/districts/ngoma.jpg",
+  nyagatare: "/districts/nyagatare.jpg",
+  rwamagana: "/districts/rwamagana.jpg",
+  gasabo: "/districts/gasabo.jpg",
+  kicukiro: "/districts/kicukiro.jpg",
+  nyarugenge: "/districts/nyarugenge.png",
+  burera: "/districts/burera.jpg",
+  gakenke: "/districts/gakenke.jpg",
+  gicumbi: "/districts/gicumbi.jpg",
+  musanze: "/districts/musanze.jpg",
+  rulindo: "/districts/rulindo.jpeg",
+  gisagara: "/districts/gisagara.webp",
+  huye: "/districts/huye.jpg",
+  kamonyi: "/districts/kamonyi.jpg",
+  muhanga: "/districts/muhanga.jpg",
+  nyamagabe: "/districts/nyamagabe.jpg",
+  nyanza: "/districts/nyanza.jpg",
+  nyaruguru: "/districts/nyaruguru.webp",
+  ruhango: "/districts/ruhango.jpg",
+  karongi: "/districts/karongi.jpg",
+  ngororero: "/districts/ngororero.jpg",
+  nyabihu: "/districts/nyabihu.jpeg",
+  nyamasheke: "/districts/nyamasheke.jpg",
+  rubavu: "/districts/rubavu.jpg",
+  rusizi: "/districts/rusizi.jpeg",
+  rutsiro: "/districts/rutsiro.jpg",
+};
